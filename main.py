42b47e2bf1f75ae1ce0cd3adc71d63c4091c48fa
KhazAkar <damianzrb@zohomail.eu>
2024-04-29 23:45:00 +0200

    Finish exercise, working!

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+def sort_on(d):
+    return d["count"]
+
 def create_list_of_dicts(letters_dict: dict[str, int]) -> list[dict[str, int]]:
     list_of_dicts = []
     for name, val in letters_dict.items():
-        list_of_dicts.append({f'{name}': val})
+        list_of_dicts.append({'letter': name, 'count': val})
     return list_of_dicts
 
 def count_letters(txt: str) -> dict[str, int]:
@@ -18,15 +21,17 @@ def count_letters(txt: str) -> dict[str, int]:
 def count_words(txt: str) -> int:
     words = txt.split()
     return len(words)
+
 def create_report(txt: str) -> None:
     print("--- Begin report of books/frankenstein.txt --- ")
-    print(f"{count_words(txt)} words found in the document")
+    print(f"{count_words(txt)} words found in the document\n")
     letters = create_list_of_dicts(count_letters(txt))
     letters.sort(reverse=True, key=sort_on)
-    for letter in letters:
-        letter_val = letters[letter]
-        print(f"The '{letter}' character was found {letter_val} times")
-
+    for letter_dict in letters:
+        letter_val = letter_dict['count']
+        letter_name = letter_dict['letter']
+        print(f"The '{letter_name}' character was found {letter_val} times")
+    print("--- End report ---")
 def main():
     content = ""
     with open('books/frankenstein.txt', encoding='utf-8') as f:
